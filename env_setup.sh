0524dcc2ee1067b50ce5565f19fb225c0d5f0afa
peter-evans <peter-evans@users.noreply.github.com>
2018-05-25 16:04:23 +0900

    Added local environment setup and test scripts

---
+++ env_setup.sh
@@ -0,0 +1,4 @@
+hererocks lua_install -r^ --lua=5.3
+./lua_install/bin/luarocks install busted
+./lua_install/bin/luarocks install luacheck
+./lua_install/bin/luarocks install luacov
---
+++ test.sh
@@ -0,0 +1,3 @@
+./lua_install/bin/luacheck --std max+busted *.lua spec
+./lua_install/bin/busted --verbose --coverage
+./lua_install/bin/luacov spec, mymodule.lua
