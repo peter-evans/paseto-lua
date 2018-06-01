03aeb778e499575f1a1574eb8ac380500d2eb1d7
peter-evans <peter-evans@users.noreply.github.com>
2018-06-01 16:10:18 +0900

    Added base64 encode decode

--- spec/utils_spec.lua
+++ spec/utils_spec.lua
@@ -11,6 +11,30 @@ describe("utils", function()
 
   end)
 
+  describe("base64 encode/decode", function()
+    
+    it("should encode and decode", function()
+      assert.equal(utils.base64_encode(""), "")
+      assert.equal(utils.base64_encode("f"), "Zg==")
+      assert.equal(utils.base64_encode("fo"), "Zm8=")
+      assert.equal(utils.base64_encode("foo"), "Zm9v")
+      assert.equal(utils.base64_decode("Zg=="), "f")
+      assert.equal(utils.base64_decode("Zm8="), "fo")
+      assert.equal(utils.base64_decode("Zm9v"), "foo")
+    end)
+
+    it("should encode and decode with no padding", function()
+      assert.equal(utils.base64_encode("", true), "")
+      assert.equal(utils.base64_encode("f", true), "Zg")
+      assert.equal(utils.base64_encode("fo", true), "Zm8")
+      assert.equal(utils.base64_encode("foo", true), "Zm9v")
+      assert.equal(utils.base64_decode("Zg"), "f")
+      assert.equal(utils.base64_decode("Zm8"), "fo")
+      assert.equal(utils.base64_decode("Zm9v"), "foo")
+    end)
+
+  end)
+
   describe("pre auth encode", function()
 
     it("should encode an empty array", function()
--- utils.lua
+++ utils.lua
@@ -27,6 +27,62 @@ local utils = {
   ]]
 }
 
+local ctable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+function utils.tohex(str)
+    return (str:gsub(".", function (c)
+        return string.format("%02x", string.byte(c))
+    end))
+end
+
+function utils.fromhex(str)
+    return (str:gsub("..", function (cc)
+        return string.char(tonumber(cc, 16))
+    end))
+end
+
+function utils.base64_encode(data, no_padding)
+    return ((data:gsub(".", function(x) 
+        local r, ctable = "", x:byte()
+        for i = 8, 1, -1 do
+          r = r .. (ctable % 2 ^ i - ctable % 2 ^ (i - 1) > 0 and "1" or "0")
+        end
+        return r
+    end) .. "0000"):gsub("%d%d%d?%d?%d?%d?", function(x)
+        if (#x < 6) then
+          return ""
+        end
+        local c = 0
+        for i = 1, 6 do
+          c = c + (x:sub(i, i) == "1" and 2 ^ (6 - i) or 0)
+        end
+        return ctable:sub(c + 1, c + 1)
+    end) .. (no_padding and "" or ({ "", "==", "=" })[#data % 3 + 1]))
+end
+
+function utils.base64_decode(data)
+    data = string.gsub(data, "[^" .. ctable .. "=]", "")
+    return (data:gsub(".", function(x)
+        if (x == "=") then
+          return ""
+        end
+        local r, f = "", (ctable:find(x) - 1)
+        for i = 6, 1, -1 do
+          r = r .. (f % 2 ^ i - f % 2 ^ (i - 1) > 0 and "1" or "0")
+        end
+        return r
+    end):gsub("%d%d%d?%d?%d?%d?%d?%d?", function(x)
+        if (#x ~= 8) then
+          return ""
+        end
+        local c = 0
+        for i = 1, 8 do
+          c = c + (x:sub(i, i) == "1" and 2 ^ (8 - i) or 0)
+        end
+        return string.char(c)
+    end))
+end
+
 function utils.pre_auth_encode(...)
   local encoded = string.pack("I8", #{...})
   for _, piece in ipairs({...})
@@ -34,18 +90,6 @@ function utils.pre_auth_encode(...)
     encoded = encoded .. string.pack("I8", #piece) .. piece
   end
   return encoded
-end
-
-function utils.tohex(str)
-    return (str:gsub('.', function (c)
-        return string.format('%02x', string.byte(c))
-    end))
-end
-
-function utils.fromhex(str)
-    return (str:gsub('..', function (cc)
-        return string.char(tonumber(cc, 16))
-    end))
 end
 
 return utils
