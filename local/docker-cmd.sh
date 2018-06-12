31f90e2da9dfc4e9896233d1cce986ec1341c378
peter-evans <peter-evans@users.noreply.github.com>
2018-06-12 12:03:56 +0900

    Added sign test vectors

--- local/docker-cmd.sh
+++ local/docker-cmd.sh
@@ -2,6 +2,6 @@
 
 luarocks make
 
-luacheck --std max+busted paseto spec
+luacheck --std max+busted --no-max-line-length paseto spec
 busted --verbose --coverage
 luacov paseto, spec
--- spec/v2_vector_spec.lua
+++ spec/v2_vector_spec.lua
@@ -4,7 +4,7 @@ local basexx = require "basexx"
 describe("v2 protocol official test vectors", function()
 
   local symmetric_key, null_key, full_key, secret_key
-  local message1, message2, footer1, footer2, nonce1, nonce2
+  local message1, message2, message3, footer1, footer2, nonce1, nonce2
 
   setup(function()
     symmetric_key = basexx.from_hex("707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f")
@@ -13,6 +13,7 @@ describe("v2 protocol official test vectors", function()
     secret_key = basexx.from_hex("b4cbfb43df4ce210727d953e4a713307fa19bb7d9f85041438d9e11b942a37741eb9dbbbbc047c03fd70604e0071f0987e16b28b757225c11f00415d0e20b1a2")
     message1 = "{\"data\":\"this is a signed message\",\"exp\":\"2019-01-01T00:00:00+00:00\"}"
     message2 = "{\"data\":\"this is a secret message\",\"exp\":\"2019-01-01T00:00:00+00:00\"}"
+    message3 = "{\"data\":\"this is a signed message\",\"expires\":\"2019-01-01T00:00:00+00:00\"}"
     footer1 = ""
     footer2 = "{\"kid\":\"zVhMiPBP9fRf2snEcT7gFTioeA9COcNy9DfgL1W60haN\"}"
     nonce1 = string.rep("\0", 24)
@@ -148,13 +149,62 @@ describe("v2 protocol official test vectors", function()
     it("should pass 'Test Vector 2E-5'", function()
       assert.equal(
         "v2.local.lClhzVOuseCWYep44qbA8rmXry66lUupyENijX37_I_z34EiOlfyuwqIIhOjF-e9m2J-Qs17Gs-BpjpLlh3zf-J37n7YGHqMBV6G5xD2aeIKpck6rhfwHpGF38L7ryYuzuUeqmPg8XozSfU4PuPp9o8.UGFyYWdvbiBJbml0aWF0aXZlIEVudGVycHJpc2Vz",
-        paseto.__encrypt(symmetric_key, "{\"data\":\"this is a signed message\",\"expires\":\"2019-01-01T00:00:00+00:00\"}", "Paragon Initiative Enterprises", nonce2))
+        paseto.__encrypt(symmetric_key, message3, "Paragon Initiative Enterprises", nonce2))
     end)
 
     it("should pass 'Test Vector 2E-6'", function()
       assert.equal(
         "v2.local.5K4SCXNhItIhyNuVIZcwrdtaDKiyF81-eWHScuE0idiVqCo72bbjo07W05mqQkhLZdVbxEa5I_u5sgVk1QLkcWEcOSlLHwNpCkvmGGlbCdNExn6Qclw3qTKIIl5-zSLIrxZqOLwcFLYbVK1SrQ.eyJraWQiOiJ6VmhNaVBCUDlmUmYyc25FY1Q3Z0ZUaW9lQTlDT2NOeTlEZmdMMVc2MGhhTiJ9",
         paseto.__encrypt(symmetric_key, message1, footer2, nonce2))
+    end)
+
+  end)
+
+  describe("sign test vectors", function()
+
+    it("should pass 'Test Vector S-1'", function()
+      assert.equal(
+        "v2.public.xnHHprS7sEyjP5vWpOvHjAP2f0HER7SWfPuehZ8QIctJRPTrlZLtRCk9_iNdugsrqJoGaO4k9cDBq3TOXu24AA",
+        paseto.sign(secret_key, ""))
+    end)
+
+    it("should pass 'Test Vector S-2'", function()
+      assert.equal(
+        "v2.public.Qf-w0RdU2SDGW_awMwbfC0Alf_nd3ibUdY3HigzU7tn_4MPMYIKAJk_J_yKYltxrGlxEdrWIqyfjW81njtRyDw.Q3VvbiBBbHBpbnVz",
+        paseto.sign(secret_key, "", "Cuon Alpinus"))
+    end)
+
+    it("should pass 'Test Vector S-3'", function()
+      assert.equal(
+        "v2.public.RnJhbmsgRGVuaXMgcm9ja3NBeHgns4TLYAoyD1OPHww0qfxHdTdzkKcyaE4_fBF2WuY1JNRW_yI8qRhZmNTaO19zRhki6YWRaKKlCZNCNrQM",
+        paseto.sign(secret_key, "Frank Denis rocks"))
+    end)
+
+    it("should pass 'Test Vector S-4'", function()
+      assert.equal(
+        "v2.public.RnJhbmsgRGVuaXMgcm9ja3qIOKf8zCok6-B5cmV3NmGJCD6y3J8fmbFY9KHau6-e9qUICrGlWX8zLo-EqzBFIT36WovQvbQZq4j6DcVfKCML",
+        paseto.sign(secret_key, "Frank Denis rockz"))
+    end)
+
+    it("should pass 'Test Vector S-5'", function()
+      assert.equal(
+        "v2.public.RnJhbmsgRGVuaXMgcm9ja3O7MPuu90WKNyvBUUhAGFmi4PiPOr2bN2ytUSU-QWlj8eNefki2MubssfN1b8figynnY0WusRPwIQ-o0HSZOS0F.Q3VvbiBBbHBpbnVz",
+        paseto.sign(secret_key, "Frank Denis rocks", "Cuon Alpinus"))
+    end)
+
+    it("should pass 'Test Vector S-6'", function()
+      assert.equal(
+        "v2.public.eyJkYXRhIjoidGhpcyBpcyBhIHNpZ25lZCBtZXNzYWdlIiwiZXhwaXJlcyI6IjIwMTktMDEtMDFUMDA6MDA6MDArMDA6MDAifSUGY_L1YtOvo1JeNVAWQkOBILGSjtkX_9-g2pVPad7_SAyejb6Q2TDOvfCOpWYH5DaFeLOwwpTnaTXeg8YbUwI",
+        paseto.sign(secret_key, message3))
+      assert.equal(
+        "v2.public.eyJkYXRhIjoidGhpcyBpcyBhIHNpZ25lZCBtZXNzYWdlIiwiZXhwaXJlcyI6IjIwMTktMDEtMDFUMDA6MDA6MDArMDA6MDAifcMYjoUaEYXAtzTDwlcOlxdcZWIZp8qZga3jFS8JwdEjEvurZhs6AmTU3bRW5pB9fOQwm43rzmibZXcAkQ4AzQs.UGFyYWdvbiBJbml0aWF0aXZlIEVudGVycHJpc2Vz",
+        paseto.sign(secret_key, message3, "Paragon Initiative Enterprises"))
+    end)
+
+    it("should pass 'Test Vector S-7'", function()
+      assert.equal(
+        "v2.public.eyJkYXRhIjoidGhpcyBpcyBhIHNpZ25lZCBtZXNzYWdlIiwiZXhwIjoiMjAxOS0wMS0wMVQwMDowMDowMCswMDowMCJ9flsZsx_gYCR0N_Ec2QxJFFpvQAs7h9HtKwbVK2n1MJ3Rz-hwe8KUqjnd8FAnIJZ601tp7lGkguU63oGbomhoBw.eyJraWQiOiJ6VmhNaVBCUDlmUmYyc25FY1Q3Z0ZUaW9lQTlDT2NOeTlEZmdMMVc2MGhhTiJ9",
+        paseto.sign(secret_key, message1, footer2))
     end)
 
   end)
